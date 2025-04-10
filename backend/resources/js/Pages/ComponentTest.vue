4ef1095e560b6af126148acdfca30e1673efd03a
Onzoro-rento <rento0725ken1009@icloud.com>
2025-04-10 18:54:16 +0900

    test completed

---
+++ backend/resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,15 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import TextInput from '@/Components/TextInput.vue';
+
+</script>
+
+<template>
+    <GuestLayout>
+        <InputLabel value="件名">タイトル</InputLabel>
+        <TextInput modelValue="何かが入ります"></TextInput>
+    </GuestLayout>
+
+</template>
+    
---
+++ backend/resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { router } from '@inertiajs/vue3';
+import InertiaTest from '../InertiaTest.vue';
+
+defineProps({
+    id : String,
+    blog : Object,
+})
+
+const deleteConfirm = (id) => {
+    // console.log(id);
+    router.delete(`/inertia/${id}`, {
+        onBefore: () => confirm('本当に削除しますか？'),
+    });
+}
+    
+</script>
+<template>
+{{ id }}<br>
+{{ blog.title }}
+<button @click="deleteConfirm(blog.id)">削除</button>
+</template>
